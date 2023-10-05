195f05daf20021a50b326c92e732e74844c0001c
vapppu <veera.hiltunen@gmail.com>
2023-10-05 10:34:17 +0300

    Add state for rendering only important or all notes, and add button to change which ones are shown on screen

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = (props) => {
 
   const[notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -22,11 +23,20 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
         <Note key = {note.id} note = {note}/>)}
       </ul>
       <form onSubmit={addNote}>
