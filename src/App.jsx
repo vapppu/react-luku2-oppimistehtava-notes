090cda16852627f974e45c67cb0dccf8e96c1873
Veera Hiltunen <veera.hiltunen@gmail.com>
2023-11-01 18:14:05 +0200

    Add error handling to toggleImportanceOf function

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,11 @@ const App = () => {
       .update(id, changedNote)
       .then((returnedNote) => {
           setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
-        });
+        })
+      .catch(error => {
+        alert(`the note ${note.content} was already deleted from server`)
+        setNotes(notes.filter(n => n.id !== id))
+      });
   };
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
